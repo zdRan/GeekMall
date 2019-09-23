f6e46f0523f991cac377c0881e805abf2ad40f8b
zdRan <cm.zdran@gmail.com>
2019-09-23 19:35:10 +0800

    添加背景

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,5 +1,27 @@
 Page({
   data: {
-    motto: 'home'
+    inputShowed: false,
+    inputVal: ""
+  },
+  showInput: function () {
+    this.setData({
+      inputShowed: true
+    });
+  },
+  hideInput: function () {
+    this.setData({
+      inputVal: "",
+      inputShowed: false
+    });
+  },
+  clearInput: function () {
+    this.setData({
+      inputVal: ""
+    });
+  },
+  inputTyping: function (e) {
+    this.setData({
+      inputVal: e.detail.value
+    });
   }
 })
